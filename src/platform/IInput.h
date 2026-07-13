355d781da8929293f19f9983395b8c5eb7d4f167
mouanes-monk <mouenis2@gmail.com>
2026-07-13 22:50:34 +0100

    feat(IInput.h): add Mouse detecting methods

--- src/platform/IInput.h
+++ src/platform/IInput.h
@@ -9,4 +9,12 @@ public:
     virtual void handleEvent(const sf::Event& event) = 0;
     virtual bool isKeyPressed(sf::Keyboard::Key key) = 0;
     virtual void update(float dt) = 0;
+    
+    // Mouse state
+    virtual float getMouseX() const = 0;
+    virtual float getMouseY() const = 0;
+    virtual float getMouseDeltaX() const = 0;
+    virtual float getMouseDeltaY() const = 0;
+    virtual float getMouseWheelDelta() const = 0;
+    virtual bool isMouseButtonPressed(int button) = 0;
 };
